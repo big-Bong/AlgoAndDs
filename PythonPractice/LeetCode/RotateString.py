4db60c648049a02c8b366ee9e1f0bec414905ac9
big-Bong <vasuchat@gmail.com>
2019-12-15 08:48:22 +0530

    Check if one string is the rotation of other

---
+++ PythonPractice/LeetCode/RotateString.py
@@ -0,0 +1,27 @@
+def rotateString(A: str, B: str) -> bool:
+    if(len(A) != len(B)):
+        return False
+    
+    i = j = start = 0
+    flag = False
+
+    while(i<len(A)):
+        if(A[i] != B[j]):
+            if(flag):
+                i = start + 1
+                j = 0
+                flag = False
+            else:
+                i += 1
+        else:
+            if(not flag):
+                start = i
+            i += 1
+            j += 1
+            flag = True
+        
+    return(B[j:]==A[:start])
+
+A = "waterbottle"
+B = "erbottlewat"
+print(rotateString(A,B))
