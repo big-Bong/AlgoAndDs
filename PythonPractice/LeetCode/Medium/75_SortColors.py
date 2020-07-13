47c98eca42dd4b5bde08f8da199c809d746b1fdd
big-Bong <vasuchat@gmail.com>
2020-07-13 13:24:03 +0530

    Sort numbers of same kind together, optimized

--- PythonPractice/LeetCode/Medium/75_SortColors.py
+++ PythonPractice/LeetCode/Medium/75_SortColors.py
@@ -1,4 +1,4 @@
-def sortColors(nums):
+def sortColorsUnoptimized(nums):
 	N = len(nums)
 	if(N<=1):
 		return nums
@@ -40,5 +40,21 @@ def sortColors(nums):
 	
 	return nums
 
-nums = [1,1,0]
+def sortColors(nums):
+	startptr, currptr, endptr = 0, 0, len(nums) - 1
+
+	while(currptr <= endptr):
+		if(nums[currptr] == 0):
+			nums[currptr],nums[startptr] = nums[startptr],nums[currptr]
+			currptr += 1
+			startptr += 1
+		elif(nums[currptr] == 1):
+			currptr += 1
+		else:
+			nums[currptr],nums[endptr] = nums[endptr],nums[currptr]
+			endptr -= 1
+
+	return nums
+
+nums = [1,2,2,0]
 print(sortColors(nums))
