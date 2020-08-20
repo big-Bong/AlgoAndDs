4f2df84b4c7bef2aaee7739a970e8c186fea90d8
big-Bong <vasuchat@gmail.com>
2020-08-20 12:38:03 +0530

    Path with sum in binary tree. Optimized.

--- PythonPractice/CTCI/4_12_PathWithSum.py
+++ PythonPractice/CTCI/4_12_PathWithSum.py
@@ -29,6 +29,42 @@ def pathSumBFHelper(node,sum,currentSum):
 
 	return totalPaths
 
+def pathSumOpt(root,currentSum):
+	new_dict = {}
+	return pathSumOptHelper(root,currentSum,0,new_dict)
+
+def pathSumOptHelper(node,currentSum,runningSum,new_dict):
+	if(not node):
+		return 0
+
+	runningSum += node.val
+	totalPaths = 0
+	diff_sum = runningSum - currentSum
+	if(diff_sum in new_dict):
+		totalPaths += new_dict[diff_sum]
+
+	if(runningSum == currentSum):
+		totalPaths += 1
+
+	updateDictionary(new_dict,runningSum,1)
+	totalPaths += pathSumOptHelper(node.left,currentSum,runningSum,new_dict)
+	totalPaths += pathSumOptHelper(node.right,currentSum,runningSum,new_dict)
+	updateDictionary(new_dict,runningSum,-1)
+
+	return totalPaths
+
+def updateDictionary(new_dict,runningSum,delta):
+	new_count = 0
+	if(runningSum in new_dict):
+		new_count = new_dict[runningSum]
+
+	new_count += delta
+	if(new_count == 0):
+		del new_dict[runningSum]
+	else:
+		new_dict[runningSum] = new_count
+
+
 
 root = Tree(10)
 root.left = Tree(5)
@@ -40,4 +76,4 @@ root.left.right.right = Tree(1)
 root.right = Tree(-3)
 root.right.right = Tree(11)
 
-print(pathSumBF(root,7))
+print(pathSumOpt(root,8))
