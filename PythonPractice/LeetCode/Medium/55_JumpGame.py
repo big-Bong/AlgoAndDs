7f79ab3a9c8723cea493c38e4fe9afec64899780
big-Bong <vasuchat@gmail.com>
2020-08-28 21:44:43 +0530

    Can we jump from 0 to last index in array. O(N)

--- PythonPractice/LeetCode/Medium/55_JumpGame.py
+++ PythonPractice/LeetCode/Medium/55_JumpGame.py
@@ -17,5 +17,23 @@ def canJump(nums):
 
 	return bool_arr[0]
 
-arr = [3,2,1,0,4]
-print(canJump(arr))
+def canJump1(nums):
+	if(not nums):
+		return False
+	N = len(nums)
+	if(N==1):
+		return True
+	max_jump = 0
+	max_jump_pos = N-1
+	for i in range(N-2,-1,-1):
+		curr = i+nums[i]
+		if(curr >= max_jump_pos):
+			max_jump_pos = i
+			val = max(curr,max_jump)
+			if(val>max_jump):
+				max_jump = val
+	return (max_jump_pos == 0 and max_jump >= N-1)
+
+
+arr = [2,0,1,0,0]
+print(canJump1(arr))
