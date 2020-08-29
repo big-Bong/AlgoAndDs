3dee50eec1725e40c0e7cdd9d277fd609a5d9301
big-Bong <vasuchat@gmail.com>
2020-08-29 19:10:09 +0530

    Rotate list. Optimized

--- PythonPractice/LeetCode/Medium/61_RotateList.py
+++ PythonPractice/LeetCode/Medium/61_RotateList.py
@@ -12,21 +12,19 @@ def rotateRight(head, k):
 	if(k==0):
 		return head
 
-	node = head
-	for _ in range(N-k-1):
-		node = node.next
+	fast_node = head
+	slow_node = head
+	for _ in range(k):
+		fast_node = fast_node.next
 
-	new_node = node.next
-	node.next = None
-	arr = []
-	while(new_node):
-		arr.append(new_node)
-		new_node = new_node.next
+	while(fast_node.next):
+		slow_node = slow_node.next
+		fast_node = fast_node.next
 
-	for i in range(len(arr)-1,-1,-1):
-		temp = arr[i]
-		temp.next = head
-		head = temp
+	temp = slow_node.next
+	slow_node.next = None
+	fast_node.next = head
+	head = temp
 
 	return head
 
@@ -46,5 +44,5 @@ def printNode(head):
 	print()
 
 
-head = ListNode(0,ListNode(1,ListNode(2)))
+head = ListNode(0,ListNode(1,ListNode(2,ListNode(3,ListNode(4,ListNode(5,ListNode(6,ListNode(7,ListNode(8,ListNode(9,ListNode(10,ListNode(11))))))))))))
 printNode(rotateRight(head,4))
