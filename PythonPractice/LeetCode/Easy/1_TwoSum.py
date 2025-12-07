7daa24bbb2f5fb574068e71f491295e11e3cd82b
big-Bong <vasuchat@gmail.com>
2025-12-07 18:24:43 +0530

    Updated two sum

--- PythonPractice/LeetCode/Easy/1_TwoSum.py
+++ PythonPractice/LeetCode/Easy/1_TwoSum.py
@@ -1,18 +1,69 @@
-from typing import List
+from typing import List, Tuple
+
+def sorting_approach(arr: List[int], target: int) -> Tuple[int,int]:
+    """
+    O(nlgn) approach to two sum
+    Runs two pointer from left and right, and depending on whether sum of elem > or < moves the pointers
+    
+    This is a wrong approach. Unless we can preserve original index positions
+    """
+    arr.sort()
+    lp = 0
+    rp = len(arr) - 1
+
+    while(lp < rp):
+        sum = arr[lp] + arr[rp]
+        if(sum == target):
+            return (lp,rp)
+        elif(sum < target):
+            lp += 1
+        else:
+            rp -= 1
+    
+    return (-1,-1)
+
+def dict_approach(arr: List[int], target: int) -> Tuple[int,int]:
+    """
+    O(n) approach to two sum. O(n) auxillary space as well
+    Use a dict to store arr numbers and indexes. Iterate over array
+    """
+
+    dict = {}
+    for (index,value) in enumerate(arr):
+        dict[value] = index
+    
+    for (index,elem) in enumerate(arr):
+        other_elem = target - elem
+        if other_elem in dict and dict[other_elem] != index:
+            return(index,dict[other_elem])
+    
+    return (-1,-1)
+
+def improved_dict_approach(nums: List[int], target: int) -> Tuple[int,int]:
+    """
+    Improved version of dict with a single loop and better variable name
+    """
+
+    lookup = {}
+    for index,elem in enumerate(nums):
+        other_elem = target-elem
+        if(other_elem in lookup):
+            return(lookup[other_elem],index)
+        lookup[elem] = index
+    
+    return (-1,-1)
+
+if __name__ == "__main__":
+    arr = [3,2,4,2]
+    target = 4
+
+    #(index1, index2) = sorting_approach(arr=arr,target=target)
+    #(index1, index2) = dict_approach(arr=arr,target=target)
+    (index1, index2) = improved_dict_approach(nums=arr,target=target)
+    if(index1 != -1):
+        print(f"The two indexes are {index1} and {index2}")
+    else:
+        print("No numbers add upto target")
 
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict = {}
-        for i in range(len(nums)):
-            dict[nums[i]] = i
-        for i in range(len(nums)):
-            other_num = target - nums[i]
-            if(other_num in dict):
-                other_num_pos = dict[other_num]
-                if(other_num_pos != i):
-                    return ([i,other_num_pos])
-        return []
 
-s = Solution()
-print(s.twoSum([3,3],6))
