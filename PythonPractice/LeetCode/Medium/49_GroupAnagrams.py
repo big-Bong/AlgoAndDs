06454dd370f14e7c1e4803ee12a23e196359dcbf
big-Bong <vasuchat@gmail.com>
2025-12-20 17:42:17 +0530

    All optimized version of group anagram

--- PythonPractice/LeetCode/Medium/49_GroupAnagrams.py
+++ PythonPractice/LeetCode/Medium/49_GroupAnagrams.py
@@ -26,7 +26,21 @@ def groupAnagrams_cleaner(strs: List[str]) -> List[List[str]]:
     
     return list(grouping.values())
 
+def groupAnagrams_optimized(strs: List[str]) -> List[List[str]]:
+    grouping = defaultdict(list) 
+	
+    for word in strs:
+        freq_arr = [0]*26
+        for ch in word:
+            freq_arr[ord(ch) - ord('a')] += 1
+		
+        #This converts the frequency array to a tuple and uses that as a key. Since anagrams will have the same frequency array, the tuple will be same for different anagrams.
+        grouping[tuple(freq_arr)].append(word)
+    
+    return list(grouping.values())
+
+
 if __name__ == "__main__":
-    #strs = ["eat","tea","tan","ate","nat","bat"]
-    strs = ["a"]
-    print(groupAnagrams(strs=strs))
+    strs = ["eat","tea","tan","ate","nat","bat"]
+    #strs = ["a"]
+    print(groupAnagrams_optimized(strs=strs))
